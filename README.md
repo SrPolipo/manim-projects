6c6c3d3c1784a8eb4869a9b3b0fd6042cd40b45b
SrPolipo <94134986+SrPolipo@users.noreply.github.com>
2026-01-19 00:05:10 +0000

    Updated Readme, updated examples.py

--- README.md
+++ README.md
@@ -5,3 +5,7 @@ This is a public repository containing a collection of my Manim scripts and anim
 ## Disclaimer
 
 Please note that the quality and organization of the code in this repository is not a priority. These scripts are primarily for personal use and experimentation with the Manim animation engine. Use at your own discretion.
+
+## Chaosgame
+
+The chaosgame folder creates a subclass of PMobject for dealing with iterated function systems, which are fractals. The Sierpinski triangle, the Vicsek fractal and a Barnsley fern are already implemented under examples.py. 
--- chaosgame/examples.py
+++ chaosgame/examples.py
@@ -86,23 +86,3 @@ def barnsley(
     ]
     return Chaosgame(function_list, vertices = vertices, transition_probabilities=probabilities, **kwargs)
 
-class Testing_Vicsek(MovingCameraScene):
-    def construct(self):
-        thing = vicsek(num_points = 20000, colors = [BLUE,WHITE,BLUE,RED,RED], stroke_width = 3, noise = 0.02)
-        self.add(thing)
-        self.camera.frame.set(height = thing.height*1.4)
-        self.wait()
-        for _ in range(7):
-            self.play(thing.animate.iterate())
-            self.wait(0.3)
-        self.wait(2)
-
-class Testing_Barnsley(MovingCameraScene):
-    def construct(self):
-        thing = barnsley(num_points = 40000, colors = [GREEN], stroke_width = 2, noise = 0.06)
-        self.add(thing)
-        self.camera.frame.shift(3*UP).scale(1.4)
-        self.wait()
-        for i in range(20):
-            self.play(thing.animate.iterate(), self.camera.frame.animate.shift(0.4/(i+1)*UP), run_time = 0.7, rate_func = rate_functions.linear)
-        self.wait(2)
